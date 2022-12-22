88d8c2402f96ba8e5dad284c2eb70b91b4668977
BryanEstrada003 <mbestrad@espol.edu.ec>
2022-12-22 17:53:05 -0500

    Actualizacion de la seccion b

---
+++ SeccionB/src/main/java/com/mycompany/seccionb/Round.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.seccionb;
+
+/**
+ *
+ * @author HOME
+ */
+public class Round implements Throw{
+
+    @Override
+    public void validar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
